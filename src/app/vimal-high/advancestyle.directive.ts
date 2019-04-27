62454fc091f57391e7b45259d32e4017fd4196f7
Vimal Pillai <vimalpillai@goodbits.in>
2019-04-27 20:41:55 +0530

    passing values to dir

--- src/app/vimal-high/advancestyle.directive.ts
+++ src/app/vimal-high/advancestyle.directive.ts
@@ -1,13 +1,25 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appAdvancestyle]'
 })
 export class AdvancestyleDirective implements OnInit {
+  @Input() defaultColor: string = 'cyan';
+  @Input('appAdvancestyle') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
 
   constructor(private elRef: ElementRef,private render: Renderer2) { }
 
   ngOnInit(){
-    this.render.setStyle(this.elRef.nativeElement,'background-color','blue');
+    //this.render.setStyle(this.elRef.nativeElement,'background-color','blue');
+    // this.backgroundColor = this.defaultColor;
+  }
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    // this.render.setStyle(this.elRef.nativeElement,'background-color','blue');
+    this.backgroundColor = this.defaultColor;
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    // this.render.setStyle(this.elRef.nativeElement,'background-color','transparent');
+    this.backgroundColor = this.highlightColor;
   }
 }
